630decec548d0d10e2935bea390c1a8301a47a04
jackytallow <3434481891@qq.com>
2019-04-14 21:09:34 +0800

    动态导航栏

---
+++ js/navigator/DynamicTabNavigator.js
@@ -0,0 +1,118 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *动态导航栏
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import PopularPage from '../page/PopularPage';
+import TrendingPage from '../page/TrendingPage';
+import FavoritePage from '../page/FavoritePage';
+import MyPage from '../page/MyPage';
+
+import {createBottomTabNavigator,
+  createAppContainer} from 'react-navigation';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import  Entypo from 'react-native-vector-icons/Entypo';
+import NavigationUtil from "../navigator/NavigationUtil";
+
+const TABS = {  //在这里配置路由
+  PopularPage:{
+    screen: PopularPage,
+    navigationOptions:{
+      tabBarLabel: '最热',
+      tabBarIcon:({tintColor ,focused}) =>(
+          <MaterialIcons
+              name={'whatshot'}
+              size={26}
+              style={{color: tintColor}}
+          />
+      ),
+    }
+  },
+  TrendingPage:{
+    screen:TrendingPage,
+    navigationOptions:{
+      tabBarLabel: '趋势',
+      tabBarIcon:({tintColor ,focused}) =>(
+          <Ionicons
+              name={'md-trending-up'}
+              size={26}
+              style={{color: tintColor}}
+          />
+      ),
+    }
+  },
+  FavoritePage:{
+    screen:FavoritePage,
+    navigationOptions:{
+      tabBarLabel: '收藏',
+      tabBarIcon:({tintColor ,focused}) =>(
+          <MaterialIcons
+              name={'favorite'}
+              size={26}
+              style={{color: tintColor}}
+          />
+      ),
+    }
+  },
+  MyPage:{
+    screen:MyPage,
+    navigationOptions:{
+      tabBarLabel: '我的',
+      tabBarIcon:({tintColor ,focused}) =>(
+          <Entypo
+              name={'user'}
+              size={26}
+              style={{color: tintColor}}
+          />
+      ),
+    }
+  }
+};
+
+export default class DynamicTabNavigator extends Component<Props> {
+  constructor(props){
+    super(props);
+    //console.disableYellowBox = true;
+  }
+
+  _tabNavigator(){
+    const {PopularPage,TrendingPage,FavoritePage,MyPage} = TABS;
+    const tabs ={PopularPage,TrendingPage,FavoritePage,MyPage} //根据需要定制显示的Tab
+    const bottomnavigator = createBottomTabNavigator(tabs,{
+         tabBarComponent:null
+        }
+    );
+    return createAppContainer(bottomnavigator);
+  }
+
+  render() {
+      NavigationUtil.navigation = this.props.navigation;
+      const Tab = this._tabNavigator();
+      return <Tab/>
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
